8ed95fafb4540b3e7d9cbcadc194e4216daed6fc
cx <kornel.k.krawczyk@o2.pl>
2022-01-06 01:08:30 +0100

    add mocha import

--- nodeFun/test/util/response.util.test.ts
+++ nodeFun/test/util/response.util.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { RespUtil } from "../../src/util/response.util";
+import "mocha";
 
 describe("Test name", () => {
   it("when x then y", () => {
